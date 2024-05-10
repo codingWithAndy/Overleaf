b217a7a5a17cedf90dd7360445d3ae47bcf8ff5b
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2024-05-10 09:05:52 +0100

    minor updates.

--- Overleaf/ViewController.swift
+++ Overleaf/ViewController.swift
@@ -59,4 +59,63 @@ class ViewController: NSViewController, WKNavigationDelegate {
             }
         }
     }
+    
 }
+
+
+
+//WebKitWebKit
+//Explore
+//To allow your app to download a PDF file from a web app like Overleaf using WebKit, you can use the WKNavigationDelegate protocol’s decidePolicyFor navigationResponse method1. This method is called each time the page wants to redirect to a new URL1.
+//
+//Here’s a simplified example of how you might implement this:
+//
+//Swift
+//
+//import WebKit
+//
+//class ViewController: UIViewController, WKNavigationDelegate {
+//    var webView: WKWebView!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        webView = WKWebView()
+//        webView.navigationDelegate = self
+//        view.addSubview(webView)
+//
+//        if let url = URL(string: "https://www.overleaf.com") {
+//            webView.load(URLRequest(url: url))
+//        }
+//    }
+//
+//    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+//        if let mimeType = navigationResponse.response.mimeType, mimeType == "application/pdf" {
+//            if let url = navigationResponse.response.url {
+//                downloadData(fromURL: url) { success, destinationURL in
+//                    if success, let destinationURL = destinationURL {
+//                        // Handle the downloaded file at destinationURL
+//                    }
+//                }
+//                decisionHandler(.cancel)
+//                return
+//            }
+//        }
+//        decisionHandler(.allow)
+//    }
+//
+//    func downloadData(fromURL url: URL, completion: @escaping (Bool, URL?) -> Void) {
+//        let downloadTask = URLSession.shared.downloadTask(with: url) { url, response, error in
+//            guard let url = url else {
+//                completion(false, nil)
+//                return
+//            }
+//            completion(true, url)
+//        }
+//        downloadTask.resume()
+//    }
+//}
+//AI-generated code. Review and use carefully. More info on FAQ.
+//In this code, the decidePolicyFor navigationResponse method checks if the MIME type of the response is "application/pdf". If it is, it downloads the data from the URL and cancels the navigation1. The downloadData(fromURL:completion:) method uses URLSession to download the file and calls the completion handler with the URL of the downloaded file1.
+//
+//Please note that this is a simplified example and you might need to adjust it according to your specific needs. For instance, you might want to handle different MIME types, add error handling, or provide a way for the user to choose where to save the downloaded file1. Also, please make sure to test this on a real device, as file downloads might not work on the iOS simulator1.
